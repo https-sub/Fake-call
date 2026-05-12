9946e08e5164b40e83e013389b53eeabff124722
https-sub <playstoreban01@gmail.com>
2026-05-12 01:40:16 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,15 @@
-self.addEventListener('install', (e) => {
-  console.log('App Installed');
+const CACHE_NAME = 'ai-puppet-v1';
+
+self.addEventListener('install', (event) => {
+    // Forces the new service worker to take over immediately
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('activate', (event) => {
+    // Clears old versions of the app
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
 });
